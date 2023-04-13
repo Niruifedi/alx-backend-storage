6953c3e8a6832c158178814ed25b7700dc9c40e3
Niruifedi <ifediniruizuchukwu@gmail.com>
2023-04-13 19:57:00 +0100

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
-
+"""
+Writing String to Redis
+"""
 import redis
 from typing import Union
 from uuid import uuid4
 
+
 class Cache():
-    
+    """
+    class Cache
+    """
     def __init__(self):
+        """
+        class constructor to instantiate redis connection
+        """
         self._redis = redis.Redis()
         self._redis.flushdb
-    
+
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        method recieves an input data and stores it with a randomly generated
+        uuid as key
+        """
         random = uuid4()
         rand = str(random)
         self._redis.set(rand, data)
