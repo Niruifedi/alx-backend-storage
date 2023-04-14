e7ec1ec7c64498c36553a9b82f6a327fddc1f9e0
Niruifedi <ifediniruizuchukwu@gmail.com>
2023-04-14 03:51:16 +0100

    redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,9 +2,26 @@
 """
 Writing String to Redis
 """
+from functools import wraps
 import redis
 from typing import Union, Callable, Optional
 from uuid import uuid4
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    counts number of calls made to Cache
+    """
+    key = method.__qualname__
+
+    @wraps(method)
+    def count(self, *args, **kwargs):
+        """
+        decorator method
+        """
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return count
 
 
 class Cache():
@@ -18,6 +35,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         method recieves an input data and stores it with a randomly generated
@@ -26,26 +44,22 @@ class Cache():
         random = uuid4()
         self._redis.set(str(random), data)
         return str(random)
-    
+
     def get(self, key: str, fn: Optional[Callable] = None) ->\
-        Union[str, bytes, int, float]:
+            Union[str, bytes, int, float]:
+        """
+        retieves value from server, convert it to desired format
+        """
         return fn(self._redis.get(key)) if fn else self._redis.get(key)
-    
+
     def get_str(self, data: bytes) -> str:
+        """
+        function recieves data as bytes and converts it to string
+        """
         return data.decode('utf-8')
-    
+
     def get_int(self, data: bytes) -> int:
+        """
+        function recieves data as bytes and converts it to integer
+        """
         return int(data)
-
-    
-cache = Cache()
-
-TEST_CASES = {
-    b"foo": None,
-    123: int,
-    "bar": lambda d:d.decode("utf-8")
-}
-
-for value, fn in TEST_CASES.items():
-    key = cache.store(value)
-    assert cache.get(key, fn=fn) == value
